2a04615982eef2c001f96ff4a476b6ee4b6cda1b
Ivan <ivan.tihonov1999@mail.ru>
2022-09-18 15:29:46 +0700

    Добавьте файлы проекта.

---
+++ Sem3_LR2/MyConsole.cpp
@@ -0,0 +1,11 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <conio.h>
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+}
