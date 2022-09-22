478a7ed4f1788b30923438b8f92b71c615c6ff3b
Ivan <ivan.tihonov1999@mail.ru>
2022-09-22 18:20:45 +0700

    Create struct Car

--- Sem3_LR2/MyConsole.cpp
+++ Sem3_LR2/MyConsole.cpp
@@ -2,10 +2,49 @@
 #include <Windows.h>
 #include <conio.h>
 #include <iostream>
+#include <string>
+
+/*
+Структура Машина:
+    Структура Информация о машина:
+        модель, 
+        цвет
+    Структура Размеры:
+        длина,
+        ширина, 
+        высота
+    Структура Характеристики:
+        коробка передач,
+        мощность двигателя,
+        объем бака,
+        максимальная скорость
+    Структура Тип машины:
+        кузов,
+        кол-во мест
+*/
+struct Car{
+    struct InfoAbout {
+        std::string model;
+        std::string color;
+    };
+    struct Size {
+        int lenght, width, high;
+    };
+    struct Parameters {
+        std::string transmission;
+        int engiinePower, tankCapacity, maxSpeed;     
+    };
+    struct TypeCar {
+        std::string typeBody;
+        int numberSeats;
+    };
+};
 
 int main()
 {
     setlocale(LC_ALL, "rus");
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+
+
 }
